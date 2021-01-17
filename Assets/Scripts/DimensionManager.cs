f6c510a80d3b8bbbc7742634a85c9185c2890ba9
fc49445 <fc49445@alunos.fc.ul.pt>
2021-01-17 17:26:22 +0000

    Created Dimension Manager

---
+++ Assets/Scripts/DimensionManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DimensionManager : MonoBehaviour
+{
+    private bool isDark;
+
+    [SerializeField]
+    public GameObject lightBackground;
+    [SerializeField]
+    public GameObject darkBackground;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDark = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SwitchLayers();
+        }
+    }
+
+    void SwitchLayers()
+    {
+        int order = lightBackground.GetComponent<TilemapRenderer>().sortingOrder;
+        lightBackground.GetComponent<TilemapRenderer>().sortingOrder = darkBackground.GetComponent<TilemapRenderer>().sortingOrder;
+        darkBackground.GetComponent<TilemapRenderer>().sortingOrder = order;
+    }
+}
